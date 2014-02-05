13d046f14652278485383e31b50ab87212e3d9ef
Duff Qiu <duffqiu@qq.com>
2014-02-05 17:16:54 +0800

    Site updated at 2014-02-05 09:16:54 UTC

---
+++ downloads/code/DesignPatternDemo/org/duffqiu/patterndemo/LazySingleton.java
@@ -0,0 +1,24 @@
+/**
+ * The file show the lazy holder
+ */
+package org.duffqiu.patterndemo;
+
+/**
+ * @author macbook
+ * 
+ *         2014年2月4日
+ */
+public class LazySingleton {
+
+    private LazySingleton() {
+
+    }
+
+    private static class LazyHolder {
+	private static final LazySingleton instance = new LazySingleton();
+    }
+
+    public static LazySingleton getInstance() {
+	return LazyHolder.instance;
+    }
+}
